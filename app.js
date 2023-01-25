e2d1aadea3475162c33fa9877b5910c7f84ca6ca
Gurkirat <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-01-25 16:25:39 +0530

    added upload

--- app.js
+++ app.js
@@ -6,14 +6,32 @@ const port = 801
 const multer =require('multer')
 //set storage engine
 const storage=multer.diskStorage({destination:'./public/upload/',filename:function(req,file,cb){
-    cb(null,);
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
 }});
+
+const upload=multer({storage:storage,limits:{fileSize:1000000}}).single('myImage');
 app.set('view engine','ejs')
 app.use(express.static('./public'))
 
 app.get('/',(req,res)=>{
     res.render('index')
 })
+app.post('/upload',(req,res)=>{
+    upload(req, res, (err) => {
+        if(err){
+          res.render('index', {
+            msg: err
+          });
+        } else {
+            console.log(req.file)
+            res.send('ff')
+        }
+      });
+    });
+    
+
+
+
 
 
 
