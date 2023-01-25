cfa2b44d4ccea2cca78f455acb4ac1504e111654
Gurkirat <63462904+SshgurkiratSingh@users.noreply.github.com>
2023-01-25 17:21:38 +0530

    last commit

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const path = require('path')
 const app = express()
 const port = 801
 const multer =require('multer')
+const { getFips } = require('crypto')
 //set storage engine
 const storage=multer.diskStorage({destination:'./public/upload/',filename:function(req,file,cb){
     cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
 }});
 
-const upload=multer({storage:storage,limits:{fileSize:1000000}}).single('myImage');
+const upload=multer({storage:storage,limits:{
+  fileSize:1000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).single('myImage');
 app.set('view engine','ejs')
 app.use(express.static('./public'))
 
@@ -23,22 +29,25 @@ app.post('/upload',(req,res)=>{
             msg: err
           });
         } else {
-            console.log(req.file)
-            res.send('ff')
+            res.render('index',{msg:"File Uploaded",file:`upload/${req.file.filename}`})
         }
       });
     });
     
 
+function checkFileType(file,cb){
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
 
-
-
-
-
-
-
-
-
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
 
 
 
